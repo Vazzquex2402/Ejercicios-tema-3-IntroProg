28f98e09d08132821cd3e2934e3d26faf5e21b2b
Santiago Vazquez <108772623+Vazzquex2402@users.noreply.github.com>
2023-02-02 21:56:59 -0300

    Delete Ejercicio Tema 3/Segunda Parte directory

--- Ejercicio Tema 3/Segunda Parte/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-
-        coche miCoche = new coche();
-        miCoche.masPuertas();
-        System.out.println(miCoche.numPuertas());
-
-    }
-}
